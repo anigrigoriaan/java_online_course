d2b30a40fe94e8d41e372aab726768f4fbc51a9a
Ani Grigorian <anigrigorriann@gmail.com>
2022-07-29 15:24:50 +0400

    My first git commit

---
+++ lesson1/lesson1/src/Lesson1.java
@@ -0,0 +1,53 @@
+public class Lesson1 {
+    public static void main(String[] args) {
+        System.out.print("Barev");
+        System.out.println("lesson 1");
+
+        byte x = 127;
+        System.out.println(x);
+        short y = -4542;
+        System.out.println(y);
+
+
+        int a = 454657;
+        System.out.println(a);
+
+        long b = 3254365477L;
+        System.out.println(b);
+
+
+        float aa = 45.4f;
+        double bb = 45.4;
+
+        boolean v = true;
+        System.out.println(v);
+        v = false;
+        System.out.println(v);
+
+        char t ='$';
+        System.out.println(t);
+
+        char tt = '\u003F';
+        System.out.println(tt);
+
+        String s = "barev lesson 1 ";
+        System.out.println(s);
+
+        String Firstname =  "Ani";
+                String lastname = "Grigorian";
+
+        System.out.println(Firstname+" "+ lastname);
+
+        int k = 95;
+        System.out.println("k= " + k);
+
+
+
+
+
+
+
+
+
+    }
+}
