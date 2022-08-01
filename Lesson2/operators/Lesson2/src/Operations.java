19a0b3c5b7b6b3adff4753806702213435ddad86
Ani Grigorian <anigrigorriann@gmail.com>
2022-08-01 20:33:57 +0400

    Commit Message

---
+++ Lesson2/operators/Lesson2/src/Operations.java
@@ -0,0 +1,107 @@
+public class Operations {
+    public static void main(String[] args) {
+        int x;
+        int $x;
+        int _x =9;
+        System.out.println(_x);
+        int a45 = 92;
+        System.out.println(a45);
+
+        String firstName = "Ani";
+        System.out.println(firstName);
+        //ORRRRRR//
+
+        String first_name = "Ani";
+        System.out.println(first_name);
+        int a = 4;
+        int b = 3;
+        int c = a+b ;
+        System.out.println(c);
+
+        System.out.println(a*b);
+
+        int d =7 , y= 5;
+        System.out.println(d/y);
+        System.out.println(d%y);
+
+        double t = 7.0 , p= 5.0;
+        System.out.println(t/p);
+
+        x=5;
+        x++;
+        System.out.println(x);
+        //x++ ORRRRR x+1////
+
+
+        ++x;
+        System.out.println(x);
+
+        //x++ ORRRR ++x ORRRR x+1///
+
+        x=4;
+        System.out.println(++x);
+        System.out.println(x);
+
+        x = 3;
+        y = 4;
+        c = x++ - --y;
+        System.out.println("x= " + x);
+        System.out.println("y= " + y);
+        System.out.println("c= " + c);
+
+        System.out.println("------------");
+
+        x = 7;
+        y = 8;
+        c = ++x + y--;
+        System.out.println(x++);
+        System.out.println(c);
+        System.out.println(x);
+        System.out.println(--y);
+        System.out.println(y);
+
+      boolean bb =   x == 9;
+        System.out.println(bb);
+        System.out.println(x != 45);
+        System.out.println(x>4);
+
+        x=41;
+        System.out.println(x% 2 ==0); //zuyga ORRRR kent///
+
+        System.out.println(x>=1 && x<= 9);
+        boolean miamish = x >=1 && x<=9;
+        boolean vv = x < 0 || x> 100;
+
+        vv = true;
+        System.out.println(!vv);
+        System.out.println("------------");
+
+
+        ///% zuyg////
+
+        System.out.println(5&8);
+        System.out.println(17&24);
+        System.out.println(5|8);
+        System.out.println(8^5);
+
+        x= 7;
+        x= x+ 2; /////////ORRRRRRRRRRR x += 2///////
+        System.out.println(x);
+        x=17;
+
+        x = x & 9;
+                x &= 9;
+
+
+
+
+
+
+
+
+
+        }
+
+
+    }
+
